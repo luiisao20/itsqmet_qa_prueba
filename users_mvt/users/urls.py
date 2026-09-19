97edb18e783eb2749ad7ca71eb450e8610057d7a
Lucho <bravo.luis.1995@gmail.com>
2026-09-18 20:28:34 -0500

    formulario para crear usuarios

--- users_mvt/users/urls.py
+++ users_mvt/users/urls.py
@@ -3,8 +3,8 @@ from .views import *
 
 urlpatterns = [
   path("", users_list, name="users_list"),
-  # path("product/<int:id>", product_detail, name="product_detail"),
-  # path("product/create", product_create, name="product_create"),
+  path("users/<int:id>", user_detail, name="user_detail"),
+  path("users/create", user_create, name="user_create"),
   # path("product/update/<int:id>", product_update, name="product_update"),
   # path("product/delete/<int:id>", product_delete, name="product_delete")
 ]
--- users_mvt/users/views.py
+++ users_mvt/users/views.py
@@ -6,4 +6,17 @@ from .forms import UserForm
 def users_list(request):
   users = User.objects.all()
   return render(request, "users/users_list.html", {"users": users})
-  
+
+def user_detail(request, id):
+  user = get_object_or_404(User, id=id)
+  return render(request, "users/user_detail.html", {"user": user})
+
+def user_create(request):
+  if request.method == 'POST':
+    form = UserForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return render(request, "users/user_detail.html", {"user": form.instance})
+  else:
+    form = UserForm()
+  return render(request, "users/user_form.html", {'form': form})
